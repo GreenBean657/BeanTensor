5b53a7e97b69bc111cbf5e8dbb586466bbf70308
GreenBean657 <greenbean@greencypher.tech>
2026-05-22 12:23:11 -0600

    Expanded Tensor Struct, but intending on diverting to class for member security.

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/schoolwork/BeanTensorV4
-# Build directory: /mnt/schoolwork/BeanTensorV4/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[BeanTensorV4Tests]=] "/mnt/schoolwork/BeanTensorV4/cmake-build-debug/BeanTensorV4Tests")
-set_tests_properties([=[BeanTensorV4Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/mnt/schoolwork/BeanTensorV4/CMakeLists.txt;165;add_test;/mnt/schoolwork/BeanTensorV4/CMakeLists.txt;0;")
